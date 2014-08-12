519041ced87a2426d37c0fb9d4db2c73a0f3da49
Tomas Vega <tomasvegagalvez@gmail.com>
2014-08-11 17:17:29 -0700

    Initial Commit

---
+++ myList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myList
+//
+//  Created by Tomas Vega on 8/11/14.
+//  Copyright (c) 2014 Tomas Vega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
